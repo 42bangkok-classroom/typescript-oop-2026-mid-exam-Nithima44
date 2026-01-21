5042e2ee4e6a27cf0b040bf14fcf0bddd15482d8
Nithima44 <nitima7859@gmail.com>
2026-01-21 14:03:31 +0700

    grading.ts

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,16 @@
 // Write your code below
+const a = Number(process.argv[2]);
+if (a<0 || a>100 || !isNaN){
+    console.log('Invalid input')
+}
+if (a>=0 && a<=49){
+    console.log('Grade is F')
+} else if(a>=50 && a<=59){
+    console.log('Grade is D')
+}else if(a>=60 && a<=69){
+    console.log('Grade is C')
+}else if(a>=70 && a<=79){
+    console.log('Grade is B')
+}else if(a>=80 && a<=100){
+    console.log('Grade is A')
+}
