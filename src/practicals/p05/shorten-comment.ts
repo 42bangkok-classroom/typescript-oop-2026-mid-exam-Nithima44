d422bf3c84f0772326663d36c0986fc2daad2c05
Nithima44 <nitima7859@gmail.com>
2026-01-21 15:45:43 +0700

    shorten-comment.ts

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,15 @@
 const comment = "TypeScript is a strongly typed programming language"
+function shortenComment(text: string): string {
+    
+  const words = text.split(' ');
+  const filteredWords = words.filter(word => word.length >= 5 && word.length <= 10);
+  if (filteredWords.length === 0) {
+    return '';
+  }
 
-function shortenComment(comment: string): string {
-  // Write your code below
+  return filteredWords.join(' ');
 }
+
+const result = shortenComment(comment);
+console.log(result);
+
