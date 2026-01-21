9283d5d79bdbdc0b1aea9c7c767d37404c08f8e7
Nithima44 <nitima7859@gmail.com>
2026-01-21 15:54:27 +0700

    unique.ts

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,19 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
-}
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+
+export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+  const combined = [...arr1, ...arr2]; 
+  const numberCount: { [key: number]: number } = {}; 
+ 
+  combined.forEach(num => {
+      numberCount[num] = (numberCount[num] || 0) + 1;
+  });
+
+  const uniqueNumbers = combined.filter(num => numberCount[num] === 1);
+
+  return uniqueNumbers; 
+}
+
+const result = getUniqueNumbers(arr1, arr2);
+console.log(result); 
