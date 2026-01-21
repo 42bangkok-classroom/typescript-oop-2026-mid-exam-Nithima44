18796bca0612cea271af416816c151626162b955
Nithima44 <nitima7859@gmail.com>
2026-01-21 16:14:15 +0700

    persons.ts

--- src/practicals/p07/persons.ts
+++ src/practicals/p07/persons.ts
@@ -1,11 +1,8 @@
 interface Person {
   firstName?: string;
   lastName?: string;
-  age?: number;
+  age?: number | null;
 }
-
-function sortPersons(persons: Person[]): Person[] {}
-
 
 const persons: Person[] = [
   { firstName: "Somchai", lastName: "Jaidee", age: 30 },
@@ -16,3 +13,42 @@ const persons: Person[] = [
   { firstName: "Jane", lastName: "Smith", age: -5 },
   { lastName: "NoName", age: 40 }
 ];
+
+function sortPersons(persons: Person[]): Person[] {
+  const filtered = persons.filter(person => 
+      person.firstName?.trim() !== '' &&
+      person.lastName?.trim() !== '' &&
+      person.age !== null && 
+      typeof person.age === 'number' && 
+      person.age >= 0 && 
+      person.age <= 100
+  );
+
+  const sorted = filtered.sort((a, b) => {
+
+      if (a.age !== undefined && b.age !== undefined && a.age!==null && b.age !==null) {
+          return a.age - b.age; 
+      }
+
+      if (a.firstName! < b.firstName!) {
+          return -1;
+      }
+      if (a.firstName! > b.firstName!) {
+          return 1;
+      }
+
+      if (a.lastName! < b.lastName!) {
+          return -1; 
+      }
+      if (a.lastName! > b.lastName!) {
+          return 1; 
+      }
+
+      return 0; 
+  });
+
+  return sorted;
+}
+
+const result = sortPersons(persons);
+console.log(result);
